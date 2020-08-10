346e3316a04a50e49ef6f43eab6f42d0c61dc59c
Ales Albert KILBERGR <iskilber@gmail.com>
2020-08-10 23:36:03 +0200

    workspace npm version updates

--- react/di/jest.config.js
+++ react/di/jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../../../coverage/libs/adk/react/di'
+  coverageDirectory: '../../../../coverage/libs/adk/react/di',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } }
 };
